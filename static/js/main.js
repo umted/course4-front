400e995fb16f22c113560740c16f47f8f7245356
umted <98448169+umted@users.noreply.github.com>
2024-02-27 20:05:35 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+let activeBlock = 'block-projects';
+const apiHost = 'http://localhost:3000';
+
+function changeActiveBlockTo(newActiveBlock) {
+    if (newActiveBlock !== activeBlock) {
+        disableCurrentBlock();
+        setActive(newActiveBlock);
+    }
+}
+
+function disableCurrentBlock() {
+    const currentBlock = document.getElementById(activeBlock);
+    currentBlock.children[1].style.transform = 'scaleY(0)';
+    currentBlock.children[1].style.overflow = 'hidden';
+    currentBlock.children[1].style.padding = 0;
+    setTimeout(() => {
+        currentBlock.children[1].style.height = 0;
+    }, 200);
+}
+
+function setActive(newActiveBlock) {
+    const newActive = document.getElementById(newActiveBlock);
+    newActive.children[1].removeAttribute('style')
+    activeBlock = newActiveBlock;
+}
+
+
+async function contacts() {
+    const contactsElem = document.getElementById('contacts__content');
+    contactsElem.innerHTML = '';
+    const contacts = await (await fetch(apiHost + '/contacts')).json();
+    for (const contact of contacts) {
+        const contactElem = document.createElement('a');
+        contactElem.href = contact.link;
+        contactElem.innerHTML = contact.name;
+        contactElem.className = 'contacts__link';
+        contactsElem.append(contactElem);
+    }
+}
+
+contacts();
